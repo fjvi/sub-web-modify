c41d785f56bd6b27beabf73bf63c2d2213339748
fjvi <fjvi@users.noreply.github.com>
2025-10-11 22:38:38 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -332,7 +332,7 @@ export default {
           "自动判断客户端": "auto",
         },
         shortTypes: {
-          "urlink": "https://urlink.pages.dev/functions/create",
+          "urlink": "https://url.mgt.xx.kg/url6889692",
           "v1.mk": "https://v1.mk/short",
           "d1.mk": "https://d1.mk/short",
           "dlj.tf": "https://dlj.tf/short",
@@ -790,7 +790,7 @@ export default {
         sourceSubUrl: "",
         clientType: "",
         customBackend: this.getUrlParam() == "" ? "https://subapi.mgt.xx.kg" : this.getUrlParam(),
-        shortType: "https://urlink.pages.dev/functions/create",
+        shortType: "https://url.mgt.xx.kg/url6889692",
         remoteConfig: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_AdblockPlus.ini",
         excludeRemarks: "",
         includeRemarks: "",
@@ -1313,5 +1313,6 @@ export default {
   }
 };
 </script>
+
 
 
